fe4c03a15afbc366a554d6c50be9805e243ae357
Daniel Lee <ddlee3@unc.edu>
2023-09-12 11:42:18 -0400

    Chardle

--- exercises/ex00_hello_world.py
+++ exercises/ex00_hello_world.py
@@ -1,2 +1,5 @@
+"""My first program for COMP110."""
+
+__author__ = "730571115"
+
 print("Hello, world.")
-# _author_=730571115
---
+++ exercises/ex01_chardle.py
@@ -0,0 +1,44 @@
+"""EX01 - Chardle - A cute step toward Wordle."""
+
+__author__ = "730571115"
+
+word = input("Enter a 5-character word:")
+
+instance = 0
+
+if len(word) != 5:
+    print("Error: Word must contain 5 characters")
+    exit()
+
+else: 
+    letter = input("Enter a single character:")
+    
+    if len(letter) != 1:
+        print("Error: Character must be a single character")
+        exit()
+    
+    else: print("Searching for " + letter + "in" + word)
+
+    if letter == word[0]:
+        print(letter + " found at index 0")
+        instance = instance + 1 
+
+    if letter == word[1]:
+        print(letter + " found at index 1")
+        instance = instance + 1
+
+    if letter == word[2]:
+        print(letter + " found at index 2")
+        instance = instance + 1
+
+    if letter == word[3]:
+        print(letter + " found at index 3")
+        instance = instance + 1
+
+    if letter == word[4]:
+        print(letter + " found at index 4")
+        instance = instance + 1 
+        
+    print(str(instance) + " instances of " + letter + " found in " + word)
+
+
